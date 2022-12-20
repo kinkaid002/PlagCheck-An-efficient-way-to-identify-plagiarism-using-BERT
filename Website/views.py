96e3d92193f9853a8ba5dbf7ca61dc5e28feedd1
Varun Kalbhore <varun.kalbhore@gmail.com>
2022-12-20 22:03:34 +0530

    Update views.py

--- Website/views.py
+++ Website/views.py
@@ -10,8 +10,8 @@ from sentence_transformers import SentenceTransformer, util
 
 views = Blueprint('views', __name__)
 
-access_key = 'AKIA2BIH4HLCO6DGGAG7'
-secret_access_key = 'B4Tc1Lps7DvMtVbVbru4VPovw1jd9Vb8DokQKl85'
+access_key = '' #Enter your AWS s3 bucket access key here
+secret_access_key = '' #Enter your AWS s3 bucket secret access key here
 
 # s3 = boto3.client('s3',
 #                     aws_access_key_id = access_key,
@@ -20,8 +20,8 @@ secret_access_key = 'B4Tc1Lps7DvMtVbVbru4VPovw1jd9Vb8DokQKl85'
 s3 = boto3.resource(
     service_name = 's3',
     region_name = 'ap-south-1',
-    aws_access_key_id = 'AKIA2BIH4HLCO6DGGAG7',
-    aws_secret_access_key = 'B4Tc1Lps7DvMtVbVbru4VPovw1jd9Vb8DokQKl85'
+    aws_access_key_id = '', #Enter your AWS s3 bucket access key here or just use the variable defined earlier "access_key"
+    aws_secret_access_key = '' #Enter your AWS s3 bucket secret access key here or just use the variable defined earlier "secret_access_key"
 )
 
 
@@ -39,20 +39,20 @@ def upload():
             filename = secure_filename(img.filename)
             img.save(filename)
             s3.Bucket('varunkalbhore-s3').upload_file(
-                # Bucket = 'varunkalbhore-s3',
+                # Bucket = 'Your S3 bucket name',
                 Filename = filename,
-                Key = 'PlagCheck/' + str(current_user.email) + '/' + str(filename)
+                Key = 'Folder name/' + str(current_user.email) + '/' + str(filename)
             )
 
-            # s3_object = s3.get_object(Bucket = 'varunkalbhore-s3', Key = 'PlagCheck/' + str(current_user.email) + '/' + str(filename))
+            # s3_object = s3.get_object(Bucket = 'Your S3 bucket name', Key = 'Folder name/' + str(current_user.email) + '/' + str(filename))
             #print(s3_object)
             
-            obj = s3.Bucket('varunkalbhore-s3').Object('PlagCheck/' + str(current_user.email) + '/' + str(filename)).get()
+            obj = s3.Bucket('#Your S3 Bucket name#').Object('Folder name/' + str(current_user.email) + '/' + str(filename)).get()
             text = obj['Body'].read().decode()
             print(text)
             
 
-            api_key = "654c38a7-428b-4a01-bc2f-d4ef9a3a18ec"
+            api_key = "" #Enter your pinecone database api-key
             pinecone.init(api_key=api_key, environment='us-west1-gcp')
             model = SentenceTransformer('all-mpnet-base-v2')
             index = pinecone.Index('sampleindex')
